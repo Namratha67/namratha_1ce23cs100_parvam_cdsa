1b6ba8b5decd80ae775617914c87f4fb241b62be
unknown <namrathanayaka6@gmail.com>
2024-11-10 15:51:26 +0530

    nov10

---
+++ nov10/linear.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int isPresent(int n,int arr[],int target){
+    for(int i=0;i<n;i++){
+        if(arr[i]==target){
+            return 1;
+        }
+    }
+    return 0;
+
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int target;
+    printf("enter the target number: ");
+    scanf("%d",&target);
+   printf("%d",isPresent(n,arr,target));
+}
